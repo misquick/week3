e9cb4bc6fa2429720cfaf5a4a1824cba570efd83
misquick <144799445+misquick@users.noreply.github.com>
2023-09-26 18:31:03 +0900

    Create task1.py

---
+++ task1.py
@@ -0,0 +1,10 @@
+try:
+    hours = int(input("Enter hours: "))
+    rate = float(input("Enter rate: "))
+    if hours <= 40:
+        pay = hours * rate
+    else:
+        pay = 40 * rate + ((hours - 40) * 1.5 * rate)
+    print("Pay:", pay)
+except ValueError:
+    print("Error, please enter numeric input.")
